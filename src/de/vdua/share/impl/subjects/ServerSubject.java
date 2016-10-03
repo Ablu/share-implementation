030b8ad5fa4114c0a6d3f5e4ee2f8592713b8495
Erik Schilling <ablu.erikschilling@gmail.com>
2016-10-03 22:14:33 +0200

    Fixed identation

--- src/de/vdua/share/impl/subjects/ServerSubject.java
+++ src/de/vdua/share/impl/subjects/ServerSubject.java
@@ -31,92 +31,92 @@ public class ServerSubject extends Subject {
     @Override
     protected void onMessageReceived(Object message) {
         try {
-                if (message instanceof StorageNodeJoinMessage) {
-                    StorageNodeJoinMessage storageNodeJoinMessage = (StorageNodeJoinMessage) message;
+            if (message instanceof StorageNodeJoinMessage) {
+                StorageNodeJoinMessage storageNodeJoinMessage = (StorageNodeJoinMessage) message;
 
-                    boolean isFirstNode = server.getStorageNodes().isEmpty();
-                    final double initialCapacity = isFirstNode ? 1.0 : 0.0;
+                boolean isFirstNode = server.getStorageNodes().isEmpty();
+                final double initialCapacity = isFirstNode ? 1.0 : 0.0;
 
-                    StorageNode newNode = new StorageNode(initialCapacity, server.getStretchFactor());
-                    synchronized (storageNodeSubjects) {
-                        storageNodeSubjects.put(newNode.getId(), storageNodeJoinMessage.subject);
-                    }
-
-                    final ConfirmJoinMessage joinMessage = new ConfirmJoinMessage();
-                    joinMessage.nodeId = newNode.getId();
-                    storageNodeJoinMessage.subject.send(joinMessage);
-
-                    HashMap<StorageNode, Double> newCapacities = new HashMap<>();
-                    for (StorageNode node : server.getStorageNodes()) {
-                        newCapacities.put(node, node.getCapacity());
-                    }
-                    newCapacities.put(newNode, initialCapacity);
-
-                    server.registerStorageNode(newNode);
-                    server.changeCapacities(newCapacities);
-                    emitChange();
-                } else if (message instanceof CapacityChangeMessage) {
-                    CapacityChangeMessage capacityChange = (CapacityChangeMessage) message;
-                    server.changeCapacities(capacityChange.newCapacities);
-                    emitChange();
-                } else if (message instanceof StretchFactorUpdateMessage) {
-                    StretchFactorUpdateMessage stretchFactorUpdate = (StretchFactorUpdateMessage) message;
-                    server.setStretchFactor(stretchFactorUpdate.stretchFactor);
-                    emitChange();
-                } else if (message instanceof StoreDataMessage) {
-                    StoreDataMessage storeData = (StoreDataMessage) message;
-                    DataEntity data = new DataEntity(storeData.data);
-                    StorageNode node = server.getStorageNodeResponsibleForStoring(data);
-
-                    StoreDataInNodeMessage storeDataInNodeMessage = new StoreDataInNodeMessage();
-                    storeDataInNodeMessage.dataId = data.getId();
-                    storeDataInNodeMessage.data = data.getData();
-                    synchronized (storageNodeSubjects) {
-                        storageNodeSubjects.get(node.getId()).send(storeDataInNodeMessage);
-                    }
-
-                    server.registerStorageLocation(data, node);
-                } else if (message instanceof DeleteMessage) {
-                    DeleteMessage deleteMessage = (DeleteMessage) message;
-                    StorageNode node = server.getStorageNodeResponsibleForStoring(deleteMessage.dataId);
-
-                    DeleteDataFromNodeMessage deleteDataFromNodeMessage = new DeleteDataFromNodeMessage();
-                    deleteDataFromNodeMessage.dataId = deleteMessage.dataId;
-                    synchronized (storageNodeSubjects) {
-                        storageNodeSubjects.get(node.getId()).send(deleteDataFromNodeMessage);
-                    }
-
-                    server.unregisterStorageLocation(deleteMessage.dataId);
-                } else if (message instanceof StorageNodeLeaveMessage) {
-                    StorageNodeLeaveMessage storageNodeLeave = (StorageNodeLeaveMessage) message;
-                    final int leavingNodeId = storageNodeLeave.nodeId;
-
-                    HashMap<StorageNode, Double> newCapacities = new HashMap<>();
-                    for (StorageNode node : server.getStorageNodes()) {
-                        final double newCapacity = node.getId() == leavingNodeId ? 0.0 : node.getCapacity();
-                        newCapacities.put(node, newCapacity);
-                    }
-                    server.changeCapacities(newCapacities);
-
-                    StorageNode nodeToDelete = null;
-                    for (StorageNode node : server.getStorageNodes()) {
-                        if (node.getId() == leavingNodeId) {
-                            nodeToDelete = node;
-                        }
-                    }
-                    server.unregisterStorageNode(nodeToDelete);
-
-                    synchronized (storageNodeSubjects) {
-                        StorageNodeSubject leavingSubject = storageNodeSubjects.get(leavingNodeId);
-                        leavingSubject.send(new ConfirmLeaveMessage());
-
-                        storageNodeSubjects.remove(leavingNodeId);
-                    }
-                    emitChange();
-                } else {
-                    final String errorMessage = "Unexpected message: " + message;
-                    throw new IllegalStateException(errorMessage);
+                StorageNode newNode = new StorageNode(initialCapacity, server.getStretchFactor());
+                synchronized (storageNodeSubjects) {
+                    storageNodeSubjects.put(newNode.getId(), storageNodeJoinMessage.subject);
                 }
+
+                final ConfirmJoinMessage joinMessage = new ConfirmJoinMessage();
+                joinMessage.nodeId = newNode.getId();
+                storageNodeJoinMessage.subject.send(joinMessage);
+
+                HashMap<StorageNode, Double> newCapacities = new HashMap<>();
+                for (StorageNode node : server.getStorageNodes()) {
+                    newCapacities.put(node, node.getCapacity());
+                }
+                newCapacities.put(newNode, initialCapacity);
+
+                server.registerStorageNode(newNode);
+                server.changeCapacities(newCapacities);
+                emitChange();
+            } else if (message instanceof CapacityChangeMessage) {
+                CapacityChangeMessage capacityChange = (CapacityChangeMessage) message;
+                server.changeCapacities(capacityChange.newCapacities);
+                emitChange();
+            } else if (message instanceof StretchFactorUpdateMessage) {
+                StretchFactorUpdateMessage stretchFactorUpdate = (StretchFactorUpdateMessage) message;
+                server.setStretchFactor(stretchFactorUpdate.stretchFactor);
+                emitChange();
+            } else if (message instanceof StoreDataMessage) {
+                StoreDataMessage storeData = (StoreDataMessage) message;
+                DataEntity data = new DataEntity(storeData.data);
+                StorageNode node = server.getStorageNodeResponsibleForStoring(data);
+
+                StoreDataInNodeMessage storeDataInNodeMessage = new StoreDataInNodeMessage();
+                storeDataInNodeMessage.dataId = data.getId();
+                storeDataInNodeMessage.data = data.getData();
+                synchronized (storageNodeSubjects) {
+                    storageNodeSubjects.get(node.getId()).send(storeDataInNodeMessage);
+                }
+
+                server.registerStorageLocation(data, node);
+            } else if (message instanceof DeleteMessage) {
+                DeleteMessage deleteMessage = (DeleteMessage) message;
+                StorageNode node = server.getStorageNodeResponsibleForStoring(deleteMessage.dataId);
+
+                DeleteDataFromNodeMessage deleteDataFromNodeMessage = new DeleteDataFromNodeMessage();
+                deleteDataFromNodeMessage.dataId = deleteMessage.dataId;
+                synchronized (storageNodeSubjects) {
+                    storageNodeSubjects.get(node.getId()).send(deleteDataFromNodeMessage);
+                }
+
+                server.unregisterStorageLocation(deleteMessage.dataId);
+            } else if (message instanceof StorageNodeLeaveMessage) {
+                StorageNodeLeaveMessage storageNodeLeave = (StorageNodeLeaveMessage) message;
+                final int leavingNodeId = storageNodeLeave.nodeId;
+
+                HashMap<StorageNode, Double> newCapacities = new HashMap<>();
+                for (StorageNode node : server.getStorageNodes()) {
+                    final double newCapacity = node.getId() == leavingNodeId ? 0.0 : node.getCapacity();
+                    newCapacities.put(node, newCapacity);
+                }
+                server.changeCapacities(newCapacities);
+
+                StorageNode nodeToDelete = null;
+                for (StorageNode node : server.getStorageNodes()) {
+                    if (node.getId() == leavingNodeId) {
+                        nodeToDelete = node;
+                    }
+                }
+                server.unregisterStorageNode(nodeToDelete);
+
+                synchronized (storageNodeSubjects) {
+                    StorageNodeSubject leavingSubject = storageNodeSubjects.get(leavingNodeId);
+                    leavingSubject.send(new ConfirmLeaveMessage());
+
+                    storageNodeSubjects.remove(leavingNodeId);
+                }
+                emitChange();
+            } else {
+                final String errorMessage = "Unexpected message: " + message;
+                throw new IllegalStateException(errorMessage);
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
