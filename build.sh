ecfabfd1a76b95b3bf17913b161addcbd2178180
Erik Schilling <ablu.erikschilling@gmail.com>
2016-10-15 21:43:50 +0200

    Added build script for solum builds

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+set -e
+
+sudo yum install -y maven
+mvn package
+sudo mkdir -p /opt/share
+sudo cp target/share-0.0.1-jar-with-dependencies.jar /opt/share/share.jar
+
+cat > /etc/systemd/share.service <<EOL
+Description=SHARE service
+
+[Service]
+ExecStart=/usr/bin/java -jar /opt/share/share.jar
+User=solum
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+systemctl enable /etc/systemd/share.service
+
