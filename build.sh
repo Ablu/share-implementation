e373df62d0f67d434d5bd48081b75b8c854d72a9
Erik Schilling <ablu.erikschilling@gmail.com>
2016-10-16 09:47:53 +0200

    Install java 1.8 for build

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+# Install java 1.8
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jdk-8u60-linux-x64.rpm"
+sudo yum localinstall jdk-8u60-linux-x64.rpm
+rm -f jdk-8u60-linux-x64.rpm
+
 sudo yum install -y maven
 mvn package
 sudo mkdir -p /opt/share
